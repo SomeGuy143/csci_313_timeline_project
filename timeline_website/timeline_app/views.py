85daeb389c4ae519f7502ca7d17325486605b4d2
EA Jacobson <emily.ann.jacobson@ndus.edu>
2023-05-02 21:14:04 -0500

    add event

--- timeline_website/timeline_app/views.py
+++ timeline_website/timeline_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from timeline_app.models import Timeline
+from timeline_app.models import Timeline, Event
 
 # Create your views here.
 def index(request):
@@ -8,5 +8,14 @@ def index(request):
 
 def timeline(request, pk):
     course = Timeline.objects.get(pk=pk)
+
+    if request.method=="POST":
+        title = request.add['title']
+        date = request.add['date']
+        description = request.add['description']
+        o = Event(date=date, title=title, 
+                  description=description, timeline_id=pk)
+        o.save()
+    
     dict = {'timeline' : timeline}
     return render(request, 'timeline_app/timeline.html', context=dict)
